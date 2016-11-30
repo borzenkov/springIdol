b634500e21737da6a1c70e22aefd16f26ea0605d
v.borzenkov <vit-borzenkov@yandex.ru>
2016-11-30 17:52:37 +0300

    2

--- src/main/java/OneManBand.java
+++ src/main/java/OneManBand.java
@@ -1,21 +1,24 @@
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * Created by vic on 30.11.16.
  */
 public class OneManBand implements Performer{
-    private Collection<Instrument> instruments;
+    private Map<String, Instrument> instruments;
 
     public OneManBand() {
     }
 
     public void perform() throws PerformanceException {
-        for (Instrument instrument : instruments) {
+        for (String key : instruments.keySet()) {
+            System.out.println(key + " : ");
+            Instrument instrument = instruments.get(key);
             instrument.play();
         }
     }
 
-    public void setInstruments(Collection<Instrument> instruments) {
+    public void setInstruments(Map<String, Instrument> instruments) {
         this.instruments = instruments;
     }
 }
